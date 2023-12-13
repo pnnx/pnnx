6c5e2d32c2d782d0c5ad891850a9ddf8038d1133
nihui <shuizhuyuanluo@126.com>
2023-12-13 22:33:50 +0800

    wip

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='pnnx-deps',
     version='0.1',
-    packages=find_packages()
+    packages=find_packages(),
+    ext_modules=[
+        setuptools.Extension(
+            name='deps',
+            sources=[]
+        )
+    ]
 )
