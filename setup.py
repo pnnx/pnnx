dfb1fcc53301d3d60abca4d94be084c58c4fcf53
nihui <shuizhuyuanluo@126.com>
2023-12-13 22:42:43 +0800

    wip

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 from setuptools import setup, find_packages
 
-try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-    class bdist_wheel(_bdist_wheel):
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            self.root_is_pure = False
-except ImportError:
-    bdist_wheel = None
+import sys
+
+sys.argv.extend(['--plat-name', 'x86_64'])
 
 setup(
     name='pnnx-deps',
