6e39ffc6de35214df5a39d73ce4a760485948b23
nihui <shuizhuyuanluo@126.com>
2023-12-13 22:45:51 +0800

    wip

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
+from setuptools.dist import Distribution
 
-import sys
-
-sys.argv.extend(['--plat-name', 'x86_64'])
+class BinaryDistribution(Distribution):
+    def has_ext_modules(foo):
+        return True
 
 setup(
     name='pnnx-deps',
     version='0.1',
-    packages=find_packages()
+    packages=find_packages(),
+    distclass=BinaryDistribution
 )
